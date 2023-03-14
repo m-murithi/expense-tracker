46459cc49729f870ba93f0fd72bc382a86630496
user name <user email>
2023-03-14 13:57:26 +0300

    outputting list of expenses

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,5 +1,11 @@
-function ExpensesList() {
-    return
+import { FlatList } from "react-native";
+
+function renderExpenseItem(itemData) {
+    return <Text>{itemData.item.description}</Text>
+}
+
+function ExpensesList({ expenses }) {
+    return <FlatList data={expenses} renderItem={renderExpenseItem}keyExtractor={(item) => item.id} />
 }
 
 export default ExpensesList;
