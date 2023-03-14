c4886a9ddb4484a99cd3e7456d9e74decef9c1d9
user name <user email>
2023-03-14 18:34:57 +0300

    implementing route parameters

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,19 @@
+import { useLayoutEffect } from 'react';
 import { Text } from 'react-native';
 
-function ManageExpense() {
-    return <Text></Text>
+function ManageExpense({route}) {
+    const editedExpenseId = route.params?.expenseId;
+    const isEditing = !!editedExpenseId;
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        });
+    }, [navigation, isEditing]);
+
+    return (
+    <Text></Text>
+    );
 }
 
 export default ManageExpense;
