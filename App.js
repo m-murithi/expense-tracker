d679d02a52aa03ee3c9d246c92dd25d9bae10921
user name <user email>
2023-03-14 12:54:49 +0300

    adding components to display expenses

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
 import { GlobalStyles } from './constants/styles';
-import { Ionicons } from '@expo/vector-icons'
+
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,5 @@
+function ExpensesList() {
+    return
+}
+
+export default ExpensesList;
